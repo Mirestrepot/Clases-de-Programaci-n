083f49326afbc2bb3d9c2da6083457e26eaafedb
Mirestrepot <92109447+Mirestrepot@users.noreply.github.com>
2023-03-21 18:29:28 -0500

    Add files via upload

---
+++ tareas/tarea1.py
@@ -0,0 +1,17 @@
+#Muestra en pantalla una variable (y) que muestre "hola mundo".
+print(y)
+#Completa el siguiente codigo para que imprima 100.
+x = 70
+
+print(x+y)
+#Completa el siguiente codigo para que imprima 2000 y explicar que pasa al sumarlo como str y como int.
+x = "1000"
+y = "1000"
+print(x+y)
+
+#con lo anterior escribe tu nombre completo terminando el siguiente codigo.
+
+
+print (nombre+apellido)
+
+#muestra en pantalla un codigo que diga tu nombre completo y despues tu edad y en que lenguaje esta codificando en estos momentos.
